a2c8864351d6e15c67ad98d0faa71f286bdca3d3
Jennifer Chang Hung Hsueh <jenhsueh15@gmail.com>
2018-07-29 23:14:27 -0700

    kata1 complete

--- kata1.js
+++ kata1.js
@@ -3,5 +3,21 @@
 // output: "hIJKLMNOPQRSTUVWXYZoPQRSTUVWXYZlMNOPQRSTUVWXYZlyZ"
 
 function insertMissingLetters (str){
-
+  let results = []
+  let alphabet = Array.from("abcdefghijklmnopqrstuvwxyz")
+  for (var i = 0; i < str.length; i++){
+    let letter = str[i] 
+    if (!results.includes(letter)) {
+      results.push(letter)
+      let startAlphabet = alphabet.indexOf(letter)
+      for(var j = startAlphabet + 1; j < alphabet.length; j++) {
+        results.push(alphabet[j].toUpperCase())
+      }
+    } else {
+      results.push(letter)
+    }
+  }
+  return results;
 }
+
+console.log(insertMissingLetters("holly").join(""))
